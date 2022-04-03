b2da1bbd79bcee5746a86103c9e36e88b8cbc33d
Bhargav Vummadi <52027911+bhargavvummadi@users.noreply.github.com>
2022-04-03 22:17:18 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from setuptools import setup, find_packages
 setup(
 	name='project1',
 	version='1.0',
-	author='You Name',
-	authour_email='your ou email',
+	author='Bhargav vummadi',
+	authour_email='bhargav.vummadi@ou.edu',
 	packages=find_packages(exclude=('tests', 'docs')),
 	setup_requires=['pytest-runner'],
 	tests_require=['pytest']
